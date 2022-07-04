82b06a81ac62c8e4da86bb46fa82c48002ff9426
marf <marf4196@outlook.com>
2022-07-04 17:23:31 +0430

    urls are now working

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Posts
 
 # Register your models here.
+class postAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'slug','description', 'created_time', 'updated_time']
+    search_fields = ['title', 'slug', 'description']
+    date_hierarchy = 'created_time'
+    ordering = ['-created_time']
+admin.site.register(Posts, postAdmin)
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def blog_view(request):
+    return HttpResponse('<h1>HEY</h1>')
