f0001ee8c51ddde24e503c133233288def71cf10
marf <marf4196@outlook.com>
2022-07-04 17:09:26 +0430

    blog model added

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Posts(models.Model):
+    title = models.CharField(max_length=256)
+    slug = models.SlugField('slug', max_length=256, unique=True, allow_unicode=True)
+    description = models.TextField()
+    image = models.ImageField(blank=True, null=True)
+    created_time = models.DateTimeField(auto_now_add=True)
+    updated_time = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'post'
+        verbose_name_plural = 'posts'
+        
+    def __str__(self):
+        return self.slug
